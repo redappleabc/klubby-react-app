651e9b221205e4809391bf2779c5c9daf0ec81df
Radomir Fugiel <radek@Radomirs-MBP.attlocal.net>
2022-02-15 16:37:35 -0600

    getWeb3

--- App.js
+++ App.js
@@ -1,10 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import getWeb3 from "./getWeb3";
 
-export default function App() {
+export default async function App() {
+  const web3 = await getWeb3();
+
+  // Use web3 to get the user's accounts.
+  const accounts = await web3.eth.getAccounts();
+
+  // Get the contract instance.
+  const networkId = await web3.eth.net.getId();
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open up App.js to start working on your app! JUDSON</Text>
       <StatusBar style="auto" />
     </View>
   );
