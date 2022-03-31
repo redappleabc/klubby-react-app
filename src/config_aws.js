9b28e8f9eb0f7e3a8b192c942f8a57e92c51c560
Brenden Judson <bjudson@trek10.com>
2022-03-31 16:41:19 -0400

    update aws config

--- src/config_aws.js
+++ src/config_aws.js
@@ -1,7 +1,7 @@
 const config = {
     region: 'us-east-1',
-    userPoolId: 'us-east-1_Ta0OOoeP6',
-    userPoolWebClientId: 'be8js4q8gts80p3ll227ut747'
+    userPoolId: 'us-east-1_O44xuYX6z',
+    userPoolWebClientId: '6irj935dduuu0lm0aslj6mo23'
 };
 
 export default config;
