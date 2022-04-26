d544765a5f6a2a8a9afef68b25f87767099c828d
redapple961129 <redapple961129@hotmail.com>
2022-04-26 17:10:11 +0900

    update config_aws.js to include both dev and prod

--- src/config_aws.js
+++ src/config_aws.js
@@ -1,7 +1,16 @@
-const config = {
-    region: 'us-east-1',
-    userPoolId: 'us-east-1_picEXpb5v',
-    userPoolWebClientId: '79f3mof59ks2p1oi9jn3thuv63'
-};
+let config_userPool;
+if(process.env.REACT_APP_ENVIRONMENT === "development"){
+    config_userPool= {
+        region: 'us-east-1',
+        userPoolId: 'us-east-1_picEXpb5v',
+        userPoolWebClientId: '79f3mof59ks2p1oi9jn3thuv63'
+    };
+}else if(process.env.REACT_APP_ENVIRONMENT === "production"){
+    config_userPool = {
+        region: 'us-east-1',
+        userPoolId: 'us-east-1_O44xuYX6z',
+        userPoolWebClientId: '6irj935dduuu0lm0aslj6mo237'
+    };
+}
 
-export default config;
+export default config_userPool;
