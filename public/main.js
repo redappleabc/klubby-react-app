8b5ed6bbb0874447edf6ad3628b3c5f2bd3ba015
okurakantaro <okura.kantaro1129@gmail.com>
2022-04-30 18:41:53 +0900

    iphone fixed

--- public/main.js
+++ public/main.js
@@ -1,6 +1,7 @@
 const appHeight = () => {
     const doc = document.documentElement
     doc.style.setProperty('--app-height', `${window.innerHeight}px`)
+    console.log(window.innerHeight);
 }
 window.addEventListener('resize', appHeight)
 appHeight()
