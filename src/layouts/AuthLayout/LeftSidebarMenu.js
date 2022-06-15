0f6e4b89b59180bdcf6ed77bdd89ee506dece7b3
okurakantaro <okura.kantaro1129@gmail.com>
2022-06-15 18:15:03 +0900

    nav finished

--- src/layouts/AuthLayout/LeftSidebarMenu.js
+++ src/layouts/AuthLayout/LeftSidebarMenu.js
@@ -49,7 +49,7 @@ function LeftSidebarMenu(props) {
                     <Nav pills className="side-menu-nav justify-content-center" role="tablist">
                        
                         <NavItem id="Klubs">
-                            <NavLink id="pills-Klubs-tab" className={classnames({ active: activeTab === 'chat' })} onClick={() => { toggleTab('chat'); }}>
+                            <NavLink id="pills-Klubs-tab" className={classnames({ active: activeTab === 'klub' })} onClick={() => { toggleTab('klub'); }}>
                                 <div>
                                     <img className='no-select' src={klubs_icon}/>
                                     <img className='select' src={klubs_icon_active}/>
